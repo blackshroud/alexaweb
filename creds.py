05ee3aa606261ebb912ed4dfdd3444a3023c71de
blackshroud <blackshroud@gmail.com>
2016-05-27 21:20:56 +0930

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,9 +1,9 @@
 #Alexa
 Security_Profile_Description ="blackshroudalexa"
-Security_Profile_ID  = ""
-Client_ID = ""
-Client_Secret = ""
-Product_ID = ""
+Security_Profile_ID  = "amzn1.application.80765665e44948dea0f6b2cca076c4b5"
+Client_ID = "amzn1.application-oa2-client.08aa8358d56b4dd3a6a9dbbd3add58e5"
+Client_Secret = "da7973280520f155b43c24240fd230bca67d843090ef89dbb1f2c228bcfaa76f"
+Product_ID = "blackshroudalexa"
 
 #Redis
-redis_url = ""
+redis_url = "redis://h:p31l1t45ifq7o07e03g0o1qd26n@ec2-54-246-90-12.eu-west-1.compute.amazonaws.com:7519"

1a1cca57aa19d4ce0cdb99b302adb8034a2f9bc5
blackshroud <blackshroud@gmail.com>
2016-05-27 19:32:40 +0930

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,4 +5,4 @@ Client_ID = "amzn1.application-oa2-client.08aa8358d56b4dd3a6a9dbbd3add58e5"
 Client_Secret = "da7973280520f155b43c24240fd230bca67d843090ef89dbb1f2c228bcfaa76f"
 
 #Redis
-redis_url = ""
+redis_url = "https://redis.heroku.com/6c73c30e-fc02-4a69-8d50-552d67bf1ef1"
